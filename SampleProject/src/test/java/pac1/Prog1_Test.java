1a1c31cf6c432962e69d1c2a8a9f4da00ccb3b88
poonamuy <poonamuy.rpee24@gmail.com>
2026-01-21 16:31:34 +0530

    newSampleproject

---
+++ SampleProject/src/test/java/pac1/Prog1_Test.java
@@ -0,0 +1,21 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class Prog1_Test {
+
+	@Test
+	public void createContactTest()
+	{
+		String URL=System.getProperty("url");
+		
+		System.out.println("Execute createcontactTest");
+	}
+
+	@Test 
+	public void modifyContactTest()
+	{
+		System.out.println("Excecute Modify contact");
+	}
+	
+}
---
+++ SampleProject/src/test/java/pac2/Prog2_Test.java
@@ -0,0 +1,19 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class Prog2_Test {
+
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("Execute createorgTest");
+	}
+
+	@Test 
+	public void modifyOrgTest()
+	{
+		System.out.println("Excecute Modify org");
+	}
+	
+}
